35b81bad7a870ba12df95c1205f44ac3b3748d91
Facarval <facarval@student.42.fr>
2025-03-28 14:38:39 +0100

    Binomial regression logistic works

--- logreg_predict.py
+++ logreg_predict.py
@@ -1,20 +1,28 @@
+import math
+
 def getThetas():
     with open("thetas.txt", 'r') as file:
         data = file.read()
         thets = data.split('\n')
         return thets
 
-def estimate(tts, age, gender, smoke_status):
-    y = tts[0] + tts[1] * age + tts[2] * gender + tts[3] * smoke_status
-    return y
+
+def sigmoid(z):
+    res = 1 / (1 + math.exp( - z))
+    return res
 
 
+def estimate(ts, age, gender, smoke_status):
+    z = ts[0] + ts[1] * age + ts[2] * gender + ts[3] * smoke_status
+    return sigmoid(z)
 
 
 def main():
     try:
         thetas = getThetas()
-        estimate(tts, )
+        for i in range(4):
+            thetas[i] = float(thetas[i])
+        print(estimate(thetas, 90, 1, 1))
     except Exception as e:
         print(e)
 
--- logreg_train.py
+++ logreg_train.py
@@ -3,6 +3,7 @@ from get_data import getData
 from logreg_predict import estimate
 import math
 
+
 def algorithm(data):
     ts = [0, 0, 0, 0]
     
@@ -12,38 +13,42 @@ def algorithm(data):
     data = data.replace(({"Gender" : map_gender}))
     data = data.replace(({"Smoker status" : map_smoker}))
     data = data.replace({"Disease" : map_diseased})
-    ages = (data["Age"] - data["Age"].mean()) / data["Age"].std()
+    ages = data["Age"]
+    genders = data["Gender"]
+    smoke_status = data["Smoker status"]
  
-    m = len(data)
     iterations = 10000
-    learning_rate = 0.001
-    res = 0
+    learning_rate = 0.0001
+    epsilon = 1e-15
     for _ in range(iterations):
         sums = [0, 0, 0, 0]
+        log_loss = 0
 
-        for i in range(m):
-            error = sigmoid(estimate(ts, ages[i], data.iloc[i]["Gender"], data.iloc[i]["Smoker status"])) - data.iloc[i]["Disease"]
-            sums[0] += error
-            sums[1] += error * ages[i]
-            sums[2] += error *  data["Gender"][i]
-            sums[3] += error * data["Smoker status"][i]
 
-        d0 = (1 / m) * sums[0]
-        d1 = (1 / m) * sums[1]
-        d2 = (1 / m) * sums[2]
-        d3 = (1 / m) * sums[3]
+        for i, status in enumerate(data["Disease"]):
+            y = status
+            ŷ = estimate(ts, ages[i], genders[i], smoke_status[i])
+            ŷ = max(epsilon, min(1 - epsilon, ŷ))
+            log_loss += y * math.log(ŷ) + (1 - y) * math.log(1 - ŷ)
+            
+            sums[0] += (ŷ - y)
+            sums[1] += (ŷ - y) * ages[i]
+            sums[2] += (ŷ - y) * genders[i]
+            sums[3] += (ŷ - y) * smoke_status[i]
 
-        ts[0] = ts[0] - (learning_rate * d0)
-        ts[1] = ts[1] - (learning_rate * d1)
-        ts[2] = ts[2] - (learning_rate * d2)
-        ts[3] = ts[3] - (learning_rate * d3)
-
+        ts[0] = ts[0] - learning_rate * sums[0]
+        ts[1] = ts[1] - learning_rate * sums[1]
+        ts[2] = ts[2] - learning_rate * sums[2]
+        ts[3] = ts[3] - learning_rate * sums[3]
+        log_loss = -log_loss
+        print(log_loss)
     return ts
 
 
-def sigmoid(z):
-    res = 1 / (1 + math.exp( - z))
-    return res
+def writeThetas(ts):
+    with open("thetas.txt" , 'w') as file:
+        file.write(f"{ts[0]}\n{ts[1]}\n{ts[2]}\n{ts[3]}")
+
 
 def main():
     try:
@@ -55,8 +60,8 @@ def main():
         data = getData('datatab.csv')
         thetas = algorithm(data)
         print(thetas)
-        print(sigmoid(estimate(thetas, 50, 0, 1)))
-
+        writeThetas(thetas)
+        print(estimate(thetas, 40, 0, 0))
     except Exception as e:
         print(e)
 
